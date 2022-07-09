b5b70c8d0530d9cfa76826655a55249a0dd496f0
Abderrahmane Fadili <abdouelrfadili@gmail.com>
2022-07-09 10:42:40 +0100

    Create Counter

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
+import { Button, Container, Row, Col } from "react-bootstrap";
 
 const App = () => {
+  const [count, setCount] = useState(0);
   return (
-    <div>
-      <h1>Hello World</h1>
-    </div>
+    <Container style={{ marginTop: 10 }}>
+      <Row className="my-3 justify-content-md-center">
+        <Col md={6}>
+          <h1 className="display-4">Counter </h1>
+        </Col>
+      </Row>
+      <Row className="justify-content-md-center">
+        <Col md={6}>
+          <Button
+            size="lg"
+            onClick={(event) => setCount(count + 1)}
+            variant="primary"
+          >
+            Count {count}
+          </Button>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
